02df1e1646a6810ae243aa0e60954bd1cf7f7215
John Saville <53898479+JohnTarvis@users.noreply.github.com>
2021-10-15 04:24:14 -0400

    Add files via upload

---
+++ jobs.js
@@ -0,0 +1,75 @@
+"use strict";
+
+
+const jsonschema = require("jsonschema");
+const express = require("express");
+const {BadRequestError} = require("../expressError");
+const {ensureAdmin} = require("../middleware/auth");
+const Job = require("../models/job");
+const jobNewSchema = require("../schemas/jobUpdate.json");
+const jobUpdateSchema = require("../schemas/jobUpdate.json");
+const jobSearchSchema = require("../schemas/jobSearch.json");
+const router = express.router({mergeParams:true});
+
+
+router.get('/',async function(req,res,next){
+    try{
+        if(!jsonschema.validate(req.query,jobSearchSchema).valid){
+            const errs = jsonschema.validate(req.query,jobSearchSchema).errors.map(e=>e.stack);
+            throw new BadRequestError(errs);
+        }
+        const jobs = await Job.findAll(q);
+        return res.json({jobs});
+    } catch(err) {
+        return next(err);
+    }
+});
+
+router.get('/:id',async function(req,res,next){
+    try{
+        const job = await Job.get(req.params.id);
+        return res.json({job});
+    } catch(err){
+        return next(err);
+    }
+});
+
+
+router.post('/',ensureAdmin, async function(req,res,next){
+    try{
+        const validator = jsonschema.validate(req.body,jobNewSchema);
+        if(!validator.valid){
+            const errs = validator.errors.map(e=>e.stack);
+            throw new BadRequestError(errs);
+        }
+        const job = await Job.create(req.body);
+        return res.status(201).json({job});
+    } catch(err){
+        return next(err);
+    }
+});
+
+router.patch("/:id",ensureAdmin,async function(req,res,next){
+    try{
+        const validator = jsonschema.validate(req.body,jobUpdateSchema);
+        if(!vvalidator.valid){
+            const errs = validator.errors.map(e=>e.stack);
+            throw new BadRequestError(errs);
+        }
+        const job = await Job.update(req.params.id,req.body);
+        return res.json({job});
+    } catch(err) {
+        return next(err);
+    }
+});
+
+router.delete("/:id",ensureAdmin,async function(req,res,next){
+    try{
+        await Job.remove(req.params.id);
+        return res.json({deleted:req.params.id});
+    } catch(err){
+        return next(err);
+    }
+})
+
+module.exports = router;
